02ad9db8c99605892dc55536c3072d7a2a60a639
Owen <owenkempton@aol.com>
2023-07-27 11:10:15 +0100

    Added sticky nav bar

--- script.js
+++ script.js
@@ -9,44 +9,49 @@ function myFunction() {
   
 function goToSummary() {
     console.log("summary")
-    let element = document.getElementById("summary");
-    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    element = document.getElementById("summary").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
 
 function goToSkills() {
     console.log("skills!")
-    let element = document.getElementById("skillsSection");
-    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    element = document.getElementById("skillsSection").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
 
 function goToEmployment() {
     console.log("employment")
-    let element = document.getElementById("employmentHistorySection");
-    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    document.getElementById("employmentHistorySection").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
 
 function goToEducation() {
     console.log("education")
-    let element = document.getElementById("educationHistory");
-    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    element = document.getElementById("educationHistory").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
 window.onhashchange = function () {
-        if(window.location.hash) {
-            let hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
-            // Hash grabbed from url in to Hash variable
-            moveToSection(hash)
-        }
+        getHashAndProceed();
+}
+
+function getHashAndProceed() {
+    if(window.location.hash) {
+        let hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+        // Hash grabbed from url in to Hash variable
+        moveToSection(hash)
+    }
 }
 
 function moveToSection(hash) {
     switch (hash){
         case "summary" :
             goToSummary();
+            break;
         case "skills" :
             goToSkills();
+            break;
         case "employment" :
             goToEmployment();
+            break;
         case "education":
             goToEducation();
     }
 }
+
+
