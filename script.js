eed61a5720e9750239096275ac0d143ccbd28a30
Owen <owenkempton@aol.com>
2023-07-27 10:48:24 +0100

    Image update and navigation improvements
    Added more text and used has in URL to navigate page

--- script.js
+++ script.js
@@ -6,3 +6,47 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+  
+function goToSummary() {
+    console.log("summary")
+    let element = document.getElementById("summary");
+    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function goToSkills() {
+    console.log("skills!")
+    let element = document.getElementById("skillsSection");
+    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function goToEmployment() {
+    console.log("employment")
+    let element = document.getElementById("employmentHistorySection");
+    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+
+function goToEducation() {
+    console.log("education")
+    let element = document.getElementById("educationHistory");
+    element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+}
+window.onhashchange = function () {
+        if(window.location.hash) {
+            let hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+            // Hash grabbed from url in to Hash variable
+            moveToSection(hash)
+        }
+}
+
+function moveToSection(hash) {
+    switch (hash){
+        case "summary" :
+            goToSummary();
+        case "skills" :
+            goToSkills();
+        case "employment" :
+            goToEmployment();
+        case "education":
+            goToEducation();
+    }
+}
